e00f97e75902e9d53f6ac3004e244fbab6e0f291
Marllon Panisset <marllon.panisset@sirius.com.br>
2018-07-12 14:26:59 -0300

    Corrigindo script de scroll para adicionar classe no navbar

--- vagrant/frontend/app/scripts/main.js
+++ vagrant/frontend/app/scripts/main.js
@@ -1,17 +1,15 @@
 (function (document, window) {
 'use strict';
 
-  window.onload = function() {
-    var navbar = document.getElementsByClassName("navbar");
-    var body = document.body;
-    var stickyHeaderTop = body.offsetTop;
-    
-    window.onscroll = function() {
-      if (window.scrollY > stickyHeaderTop) {
-        document.getElementsByClassName("navbar").setAttribube("class","fixed");
-      } else {
-        document.getElementsByClassName("navbar").classList.remove("fixed");
-      }
-    };
+  var navbar = document.getElementsByClassName("navbar")[0];
+  var body = document.body;
+  var stickyHeaderTop = body.offsetTop;
+
+  window.onscroll = function() {
+    if (window.scrollY > 150) {
+      navbar.classList.add("scrolled");
+    } else {
+      navbar.classList.remove("scrolled");
+    }
   };
 })(document, window);
