7aacc350df2c111b489a12e2599e1b56ae48993e
Marllon Panisset <marllon.panisset@sirius.com.br>
2018-07-13 10:46:13 -0300

    Iniciando script de menu animado.

--- vagrant/frontend/app/scripts/main.js
+++ vagrant/frontend/app/scripts/main.js
@@ -1,12 +1,26 @@
 (function (document, window) {
 'use strict';
 
+  window.slide = new SlideNav();
+  
+  var nav = new SlideNav({
+    activeClass: "active",
+    toggleButtonSelector: false,
+    toggleBoxSelector: false,
+    hideAfterSelect: true,
+    speed: 700000,
+    changeHash: false,
+    navBoxToggleClass: false
+  });
+
   var navbar = document.getElementById("navbar");
 
   window.onscroll = function() {
     if (window.scrollY > 150) {
       navbar.classList.add("scrolled");
-    } else {
+    }
+    
+    else {
       navbar.classList.remove("scrolled");
     }
   };
