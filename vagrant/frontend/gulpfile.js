4819350ffbf2e9398de7b7f8b213ea0b4cdc4faa
Marllon Panisset <marllon.panisset@sirius.com.br>
2018-07-16 11:37:25 -0300

    htmlmin

--- vagrant/frontend/gulpfile.js
+++ vagrant/frontend/gulpfile.js
@@ -118,7 +118,7 @@ gulp.task('html', ['styles', 'styles:vendor', 'scripts', 'views'], () => {
 	.pipe($.if(/\.js$/, $.uglify({compress: {drop_console: true}})))
 	.pipe($.if(/\.css$/, $.cssnano({safe: true, autoprefixer: false})))
 	.pipe($.if(/\.html$/, $.htmlmin({
-	  collapseWhitespace: false,
+	  collapseWhitespace: true,
 	  minifyCSS: true,
 	  minifyJS: {compress: {drop_console: false}},
 	  processConditionalComments: false,
