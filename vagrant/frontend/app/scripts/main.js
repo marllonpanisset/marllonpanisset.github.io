337d749b38b2df5bf8fb27a6acf76fecf4bc94e0
Marllon Panisset <marllon.panisset@sirius.com.br>
2018-07-25 11:49:14 -0300

    Deploy

--- vagrant/frontend/app/scripts/main.js
+++ vagrant/frontend/app/scripts/main.js
@@ -1,12 +1,12 @@
 (function (document, window) {
 'use strict';
 
-  var navbar = document.getElementById('navbar'),
-      menuBar = document.getElementById('menu-bar'),
-      mainMenu = document.getElementsByClassName('main-menu')[0];
-
+  var navbar = document.getElementById('navbar');
+  var menuBar = document.getElementById('menu-bar');
+  var mainMenu = document.getElementsByClassName('main-menu')[0];
       
   window.onscroll = function() {
+
     if (window.scrollY > 150) {
       navbar.classList.add('scrolled');
     }
@@ -21,42 +21,46 @@
     if(mainMenu.classList.contains('open')) {
       mainMenu.classList.remove('open');
     }
-    
+
     else {
       mainMenu.classList.add('open');
     }
   };
   
   [].forEach.call(document.querySelectorAll('ul.main-menu li a, h1.site-name a'), function (el) {
+
     el.addEventListener('click', function () {
+
       if (mainMenu.classList.contains('open')) {
         mainMenu.classList.remove('open');
       }
     });
   });
-  
+
   $('[data-scroll]').click(function (event) {
     event.preventDefault();
-    var $this = $(this),
-      scroll = $this.data('scroll');
+    var $this = $(this);
+    var scroll = $this.data('scroll');
+
     if ($(scroll).length > 0) {
       var top = $(scroll).offset().top;
+
       $('html,body').animate({
         scrollTop: top
       }, 1000)
     }
   });
 
-  var sections = $('.page-section'),
-      nav = $('#navbar .main-menu'),
-      nav_height = nav.outerHeight();
+  var sections = $('.page-section');
+  var nav = $('#navbar .main-menu');
+  var nav_height = nav.outerHeight();
 
   $(window).on('scroll', function () {
     var cur_pos = $(this).scrollTop();
 
     sections.each(function () {
-      var top = $(this).offset().top - nav_height,
-        bottom = top + $(this).outerHeight();
+      var top = $(this).offset().top - nav_height;
+      var bottom = top + $(this).outerHeight();
 
       if (cur_pos >= top && cur_pos <= bottom) {
         nav.find('a').removeClass('active');
@@ -69,8 +73,8 @@
   });
 
   nav.find('a').on('click', function () {
-    var $el = $(this)
-      , id = $el.attr('href');
+    var $el = $(this);
+    var id = $el.attr('href');
 
     $('html, body').animate({
       scrollTop: $(id).offset().top
