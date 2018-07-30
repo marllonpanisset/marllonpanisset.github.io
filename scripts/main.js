78b4b3ff7b5325bd7fa0654b4162bb1b2af5a820
Marllon Panisset <marllonop@gmail.com>
2018-07-29 21:35:15 -0300

    Deploy

--- scripts/main.js
+++ scripts/main.js
@@ -3,29 +3,33 @@
 !function (t, e) {
   "use strict";
   var a = t.getElementById("navbar"),
-      s = t.getElementById("menu-bar"),
-      n = t.getElementsByClassName("main-menu")[0];e.onscroll = function () {
+      n = t.getElementById("menu-bar"),
+      s = t.getElementsByClassName("main-menu")[0];e.onscroll = function () {
     e.scrollY > 150 ? a.classList.add("scrolled") : a.classList.remove("scrolled");
-  }, s.onclick = function (t) {
-    n.classList.contains("open") ? n.classList.remove("open") : n.classList.add("open");
+  }, n.onclick = function (t) {
+    s.classList.contains("open") ? s.classList.remove("open") : s.classList.add("open");
   }, [].forEach.call(t.querySelectorAll("ul.main-menu li a, h1.site-name a"), function (t) {
     t.addEventListener("click", function () {
-      n.classList.contains("open") && n.classList.remove("open");
+      s.classList.contains("open") && s.classList.remove("open");
     });
   }), $("[data-scroll]").click(function (t) {
     t.preventDefault();var e = $(this),
         a = e.data("scroll");if ($(a).length > 0) {
-      var s = $(a).offset().top;$("html,body").animate({ scrollTop: s }, 1e3);
+      var n = $(a).offset().top;$("html,body").animate({ scrollTop: n }, 1e3);
     }
-  });var o = $(".page-section"),
-      i = $("#navbar .main-menu"),
-      l = i.outerHeight();$(e).on("scroll", function () {
-    var t = $(this).scrollTop();o.each(function () {
+  });var i = $(".page-section"),
+      o = $("#navbar .main-menu"),
+      l = o.outerHeight();$(e).on("scroll", function () {
+    var t = $(this).scrollTop();i.each(function () {
       var e = $(this).offset().top - l,
-          a = e + $(this).outerHeight();t >= e && t <= a && (i.find("a").removeClass("active"), o.removeClass("active"), $(this).addClass("active"), i.find('a[href="#' + $(this).attr("id") + '"]').addClass("active"));
+          a = e + $(this).outerHeight();t >= e && t <= a && (o.find("a").removeClass("active"), i.removeClass("active"), $(this).addClass("active"), o.find('a[href="#' + $(this).attr("id") + '"]').addClass("active"));
     });
-  }), i.find("a").on("click", function () {
+  }), o.find("a").on("click", function () {
     var t = $(this),
         e = t.attr("href");return $("html, body").animate({ scrollTop: $(e).offset().top }, 1e3), !1;
+  }), jQuery(t).ready(function () {
+    jQuery(".skillbar").each(function () {
+      jQuery(this).find(".skillbar-bar").animate({ width: jQuery(this).attr("data-percent") }, 6e3);
+    });
   });
 }(document, window);
--- vagrant/frontend/app/scripts/main.js
+++ vagrant/frontend/app/scripts/main.js
@@ -83,4 +83,12 @@
     return false;
   });
 
+  jQuery(document).ready(function(){
+    jQuery('.skillbar').each(function(){
+      jQuery(this).find('.skillbar-bar').animate({
+        width:jQuery(this).attr('data-percent')
+      },6000);
+    });
+  });
+
 })(document, window);
