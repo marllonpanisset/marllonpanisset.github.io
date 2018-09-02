a6b8a45b4648a0a35d16e8360285582ddea55058
Marllon Panisset <marllonop@gmail.com>
2018-09-02 01:06:00 -0300

    Deploy

--- vagrant/frontend/app/service-worker.js
+++ vagrant/frontend/app/service-worker.js
@@ -12,35 +12,3 @@ var filesToCache = [
     "https://i.imgur.com/mXUXjyM.jpg",
     "https://i.imgur.com/nrVdVaW.jpg"
 ];
-
-self.addEventListener('install', function (event) {
-    var indexPage = new Request('index.html');
-    event.waitUntil(
-        fetch(indexPage).then(function (response) {
-            return caches.open('pwa-offline').then(function (cache) {
-                return cache.addAll(filesToCache);
-            });
-        }));
-});
-
-self.addEventListener('fetch', function (event) {
-    var updateCache = function (request) {
-        return caches.open('pwa-offline').then(function (cache) {
-            return cache.put(request, response);
-        });
-            
-    };
-
-    event.waitUntil(updateCache(event.request));
-
-    event.respondWith(
-        fetch(event.request).catch(function (error) {
-            return caches.open('pwa-offline').then(function (cache) {
-                return cache.match(event.request).then(function (matching) {
-                    var report = !matching || matching.status == 404 ? Promise.reject('no-match') : matching;
-                    return report
-                });
-            });
-        })
-    );
-})
