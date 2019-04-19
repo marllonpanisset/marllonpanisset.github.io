3b598cd69bde15a3dc5fca2a6891d0ce9936e53d
Marllon Panisset <marllonop@gmail.com>
2019-04-19 14:01:56 -0300

    Removendo base docker

--- app/src/pages/Mensagem.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from 'react';
-
-export default class Mensagem extends Component {
-    render() {
-        return (
-            <p>Obrigado pela mensagem</p>
-        );
-    }
-}
