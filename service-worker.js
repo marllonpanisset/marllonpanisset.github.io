28544a164d37e0309647cd38a48b021ae0ac2a06
Marllon Panisset <marllon.panisset@ed3.com.br>
2022-06-29 20:29:10 -0300

    deploy service worker and manifest.json

---
+++ service-worker.js
@@ -0,0 +1,12 @@
+var filesToCache = [
+    "/index.html",
+    "favicon.ico",
+    "manifest.json",
+    "styles/main.css",
+    "js/main.js",
+    "js/ie7.js",
+    "https://i.imgur.com/i2dvomF.jpg",
+    "https://i.imgur.com/XFwAUR3.jpg",
+    "https://i.imgur.com/Jmfinns.jpg"
+
+];
