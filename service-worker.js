724beb63caa455f27e0e11d77a9366a589763b5b
Marllon Panisset <marllonop@gmail.com>
2024-12-07 00:57:54 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -5,7 +5,7 @@ var filesToCache = [
     "styles/main.css",
     "js/main.js",
     "js/ie7.js",
-    "https://i.imgur.com/pbXYgG0.png",
+    "https://i.imgur.com/NckgGg1.jpeg",
     "https://i.imgur.com/XFwAUR3.jpg",
     "https://i.imgur.com/Jmfinns.jpg"
 
