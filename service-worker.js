2014594523b018901b16bdaf100abb1cafd24c85
Marllon Panisset <panisset.dev@gmail.com>
2024-12-12 19:16:04 -0300

    alterando diretorio de imagens

--- service-worker.js
+++ service-worker.js
@@ -5,8 +5,8 @@ var filesToCache = [
     "styles/main.css",
     "js/main.js",
     "js/ie7.js",
-    "images/maquina-de-escrever.webp",
-    "images/notebook.webp",
-    "images/perfil-8080webp"
+    "/images/maquina-de-escrever.webp",
+    "/images/notebook.webp",
+    "/images/perfil-8080webp"
 
 ];
