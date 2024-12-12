1fe72868c89d514a9417b41a2904c7306d40f046
Marllon Panisset <marllonop@gmail.com>
2024-12-12 10:04:08 -0300

    alterando imagem de perfil

--- service-worker.js
+++ service-worker.js
@@ -5,7 +5,7 @@ var filesToCache = [
     "styles/main.css",
     "js/main.js",
     "js/ie7.js",
-    "https://i.imgur.com/Zy79FDh.png",
+    "https://i.imgur.com/ON49jEq.jpeg",
     "https://i.imgur.com/sfEdNf4.png",
     "https://i.imgur.com/Zutjovc.png"
 
