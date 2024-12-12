932a0fc9f9711b13fb79624126b1b99732c92d46
Marllon Panisset <panisset.dev@gmail.com>
2024-12-12 19:13:54 -0300

    ajustes

--- service-worker.js
+++ service-worker.js
@@ -5,8 +5,8 @@ var filesToCache = [
     "styles/main.css",
     "js/main.js",
     "js/ie7.js",
-    "https://i.imgur.com/ON49jEq.jpeg",
-    "https://i.imgur.com/sfEdNf4.png",
-    "https://i.imgur.com/Zutjovc.png"
+    "images/maquina-de-escrever.webp",
+    "images/notebook.webp",
+    "images/perfil-8080webp"
 
 ];
