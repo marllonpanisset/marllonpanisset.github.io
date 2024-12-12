83b284c8096373e9a4a78e258269426e96440c18
Marllon Panisset <marllonop@gmail.com>
2024-12-12 18:13:56 -0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,99 @@
-"use strict";var header=document.getElementById("header"),sectionHome=document.querySelector("#home"),isInViewport=function(e){e=e.getBoundingClientRect();return 0<=e.top&&0<=e.left&&e.bottom<=(window.innerHeight||document.documentElement.clientHeight)&&e.right<=(window.innerWidth||document.documentElement.clientWidth)};const scrollElements=document.querySelectorAll(".js-scroll"),elementInView=(e,t=1)=>{return e.getBoundingClientRect().top<=(window.innerHeight||document.documentElement.clientHeight)/t},elementOutofView=e=>{return e.getBoundingClientRect().top>(window.innerHeight||document.documentElement.clientHeight)},displayScrollElement=e=>{e.classList.add("scrolled")},hideScrollElement=e=>{e.classList.remove("scrolled")},handleScrollAnimation=()=>{scrollElements.forEach(e=>{elementInView(e,1.25)?displayScrollElement(e):elementOutofView(e)&&hideScrollElement(e)})};function onScroll(e){isInViewport(sectionHome)?header.classList.remove("fixed-header"):header.classList.add("fixed-header")}function hamburguer(e){const t=e.target;(t.matches("button#icon-menu")||t.closest("button#icon-menu"))&&(document.getElementById("menu-mobile").classList.toggle("open"),document.getElementById("icon-menu").classList.toggle("active"))}window.addEventListener("scroll",()=>{handleScrollAnimation()}),document.addEventListener("scroll",onScroll),document.getElementById("icon-menu").addEventListener("click",hamburguer),document.addEventListener("DOMContentLoaded",function(){for(const e of document.querySelectorAll(".nav-link"))e.addEventListener("click",function(e){(e.target.matches("a.nav-link")||e.target.matches("#navbar a.logo"))&&(document.getElementById("icon-menu").classList.remove("active"),document.getElementById("menu-mobile").classList.remove("open"))})});
+// Autor: Marllon Panisset
+
+"use strict"; 
+
+// ## Global Vars
+var header = document.getElementById('header');
+var sectionHome = document.querySelector('#home');
+
+// ## isInViewport
+var isInViewport = function (elem) {
+	var distance = elem.getBoundingClientRect();
+	return (
+		distance.top >= 0 &&
+		distance.left >= 0 &&
+		distance.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+		distance.right <= (window.innerWidth || document.documentElement.clientWidth)
+	);
+};
+
+// animate on Scroll 
+const scrollElements = document.querySelectorAll(".js-scroll");
+
+const elementInView = (el, dividend = 1) => {
+  const elementTop = el.getBoundingClientRect().top;
+
+  return (
+    elementTop <=
+    (window.innerHeight || document.documentElement.clientHeight) / dividend
+  );
+};
+
+const elementOutofView = (el) => {
+  const elementTop = el.getBoundingClientRect().top;
+
+  return (
+    elementTop > (window.innerHeight || document.documentElement.clientHeight)
+  );
+};
+
+const displayScrollElement = (element) => {
+  element.classList.add("scrolled");
+};
+
+const hideScrollElement = (element) => {
+  element.classList.remove("scrolled");
+};
+
+const handleScrollAnimation = () => {
+  scrollElements.forEach((el) => {
+    if (elementInView(el, 1.25)) {
+      displayScrollElement(el);
+    } else if (elementOutofView(el)) {
+      hideScrollElement(el)
+    }
+  })
+}
+
+window.addEventListener("scroll", () => { 
+  handleScrollAnimation();
+});
+
+// NAVBAR 
+function onScroll(event) {
+  if (isInViewport(sectionHome)) {
+    header.classList.remove("fixed-header");
+
+  } else {
+    header.classList.add("fixed-header");
+  }
+}
+
+document.addEventListener('scroll', onScroll);
+
+document.getElementById('icon-menu').addEventListener('click', hamburguer)
+
+function hamburguer(e) {
+  const tgt = e.target;
+  if (tgt.matches("button#icon-menu") || tgt.closest("button#icon-menu")) {
+    document.getElementById('menu-mobile').classList.toggle('open')
+    document.getElementById('icon-menu').classList.toggle('active')
+  }
+}
+
+
+document.addEventListener(
+  'DOMContentLoaded',
+  function() {
+    const links = document.querySelectorAll('.nav-link');
+
+    for (const link of links) {
+      link.addEventListener('click', function(event) {
+        if (event.target.matches('a.nav-link') || event.target.matches('#navbar a.logo')) {
+          document.getElementById('icon-menu').classList.remove('active');
+          document.getElementById('menu-mobile').classList.remove('open');
+        }
+      })
+    }
+  }
+);
