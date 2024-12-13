9b85a756c3f76967c726eee3f86aeefd56d5b70a
Marllon Panisset <panisset.dev@gmail.com>
2024-12-13 20:27:17 -0300

    criando next.config.js comando de build, e subindo pasta dist pro diretorio /portfolio/dist

---
+++ portfolio/next.config.js
@@ -0,0 +1,14 @@
+
+
+/**
+ * @type {import('next').NextConfig}
+ */
+const nextConfig = {
+    output: 'export',
+    distDir: 'dist',
+    images: {
+        unoptimized: true,
+    }
+  }
+   
+  module.exports = nextConfig
